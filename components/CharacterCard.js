d162e149e2e4c77306d6ce0ebf534dcfa244c536
Miguel <miguelarturo@gmail.com>
2025-09-20 19:45:51 -0600

    progress

--- components/CharacterCard.js
+++ components/CharacterCard.js
@@ -10,6 +10,7 @@ import {
 } from "@/components/ui/dialog"
 import { Button } from "@/components/ui/button"
 import { fetchCharacterDetails } from '../lib/swapi';
+import { supabase } from '../lib/client';
 
 const CharacterCard = ({ character, films, starships }) => {
   const { addFavorite, removeFavorite, favoriteCharacters, isFavorite } = useStore();
@@ -21,12 +22,58 @@ const CharacterCard = ({ character, films, starships }) => {
   const toggleFavorite = () => {
     if (isFavorite(character.name)) {
       let updatedFavorites = removeFavorite(character);
+      // toggleFav(character.name, false)
+      deleteFav(character.name)
       localStorage.setItem('favorites', updatedFavorites);
     } else {
       let updatedFavorites = addFavorite(character);
+      addFav(character.name)
       localStorage.setItem('favorites', updatedFavorites);
     }
   };
+
+  const addFav = async (nameChar) => {
+      const newChar = {
+        name: nameChar,
+        isCompleted: true,
+      };
+      const { data, error } = await supabase
+        .from("starwars")
+        .insert([newChar])
+        .single();
+
+      if (error) {
+        console.log("Error adding todo: ", error);
+      } else {
+        // setData((prev) => [...prev, data]);
+        // setNewTodo("");
+      }
+    };
+
+    const toggleFav = async (name, isCompleted) => {
+      const { data, error } = await supabase
+        .from("starwars")
+        .update({ isCompleted: !isCompleted })
+        .eq("name", name);
+
+      if (error) {
+        console.log("error toggling task: ", error);
+      }
+    };
+
+    const deleteFav = async (name) => {
+      const { data, error } = await supabase
+        .from("starwars")
+        .delete()
+        .eq("name", name);
+
+      if (error) {
+        console.log("error deleting char: ", error);
+      } 
+      // else {
+      //   setTodoList((prev) => prev.filter((todo) => todo.id !== id));
+      // }
+    };
 
   const fetchDetails = async () => {
     const data = await fetchCharacterDetails(character.url);
@@ -139,3 +186,106 @@ const CharacterCard = ({ character, films, starships }) => {
 };
 
 export default CharacterCard;
+
+
+/*
+
+import { useEffect, useState } from "react";
+import "./App.css";
+import supabase from "./supabase-client";
+
+function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = async () => {
+    const { data, error } = await supabase.from("TodoList").select("*");
+    if (error) {
+      console.log("Error fetching: ", error);
+    } else {
+      setTodoList(data);
+    }
+  };
+
+  const addTodo = async () => {
+    const newTodoData = {
+      name: newTodo,
+      isCompleted: false,
+    };
+    const { data, error } = await supabase
+      .from("TodoList")
+      .insert([newTodoData])
+      .single();
+
+    if (error) {
+      console.log("Error adding todo: ", error);
+    } else {
+      setTodoList((prev) => [...prev, data]);
+      setNewTodo("");
+    }
+  };
+
+  const completeTask = async (id, isCompleted) => {
+    const { data, error } = await supabase
+      .from("TodoList")
+      .update({ isCompleted: !isCompleted })
+      .eq("id", id);
+
+    if (error) {
+      console.log("error toggling task: ", error);
+    } else {
+      const updatedTodoList = todoList.map((todo) =>
+        todo.id === id ? { ...todo, isCompleted: !isCompleted } : todo
+      );
+      setTodoList(updatedTodoList);
+    }
+  };
+
+  const deleteTask = async (id) => {
+    const { data, error } = await supabase
+      .from("TodoList")
+      .delete()
+      .eq("id", id);
+
+    if (error) {
+      console.log("error deleting task: ", error);
+    } else {
+      setTodoList((prev) => prev.filter((todo) => todo.id !== id));
+    }
+  };
+
+  return (
+    <div>
+      {" "}
+      <h1>Todo List</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="New Todo..."
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button onClick={addTodo}> Add Todo Item</button>
+      </div>
+      <ul>
+        {todoList.map((todo) => (
+          <li>
+            <p> {todo.name}</p>
+            <button onClick={() => completeTask(todo.id, todo.isCompleted)}>
+              {" "}
+              {todo.isCompleted ? "Undo" : "Complete Task"}
+            </button>
+            <button onClick={() => deleteTask(todo.id)}> Delete Task</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
+*/
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,36 @@
+"use client"
 import CharacterList from '../../components/CharacterList';
 import CharacterFavorites from '../../components/CharacterFavorites';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
+import { useEffect, useState } from 'react';
+import { supabase } from '../../lib/client';
+
 const Home = () => {
+  const [data, setData] = useState([]);
+
+    useEffect(() => {
+      async function fetchData() {
+        // const { data, error } = await supabase.from('starwars').select('*').ilike("name", "%A%");
+        // const { data, error } = await supabase.from('starwars').select('*').like("name", "%A%");
+        const { data, error } = await supabase.from('starwars').select('*');
+        if (error) console.error('Error fetching data:', error.message);
+        else setData(data);
+        // addTodo("Anakin")
+      }
+      fetchData();
+    }, []);
+
   return (
     <div className="min-h-screen">
+      <div>
+          <h1 className="text-4xl font-semibold mb-8">Chars in Supabase</h1>
+          <ul>
+            {data.map((item) => (
+              <li key={item.id}>{item.name} {item.isCompleted ? "FAV": "NOT FAV"}</li>
+            ))}
+          </ul>
+        </div>
       <main className="container mx-auto p-6">
         <h1 className="text-4xl font-semibold text-center mb-8">Star Wars Characters</h1>
         <Tabs defaultValue="characters" >
